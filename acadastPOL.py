bd8cab5ec9c025e738c577d8fe94fdbc15e27813
ekfar <68909674+ekfar48@users.noreply.github.com>
2021-10-26 16:08:20 +0300

    Update acadastPOL.py

--- acadastPOL.py
+++ acadastPOL.py
@@ -1,3 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
